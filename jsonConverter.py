671e6803dab3ab4e343978728ac0fa23cc2b084b
slelle <slelle@sorensonmedia.com>
2018-11-10 15:40:24 -0700

    Fixed the script to parse the data into nested json

--- jsonConverter.py
+++ jsonConverter.py
@@ -1,25 +1,45 @@
 import json
 
 
-def get_folders(array):
-    folders = []
-    for index in array:
-        if index["type"] == "folder":
-            folders.append(index)
+def get_primary(data):
+    names = [item["name"] for item in data]
+    parents = [item["parent"] for item in data]
+    for parent in parents:
+        if parent not in names:
+            return parent
+
+
+def get_names(data):
+    return [item["name"] for item in data]
+
+
+def set_main_folder(data, primary):
+    for item in data:
+        if item["parent"] == primary and item["type"] == "folder":
+            item.update({"children": []})
+            return item
+
+
+def set_children(data, names, primary):
+    folders = set_main_folder(data, primary)
+    for i in data:
+        if i["parent"] in names:
+            parent_name = i["parent"]
+            for x in data:
+                if x["name"] == parent_name and "children" not in x and x["type"] == "folder":
+                    x.update({"children": []})
+                if x["name"] == parent_name and "children" in x:
+                    object = x["children"]
+                    object.append(i)
+                    x.update({"children": object})
     return folders
-
-
-def check_parents(data, array):
-    parents = []
-    for item in array:
-        parents.append()
 
 
 def main(inputFile, outputFile):
     with open(inputFile, 'r') as input:
         data = json.load(input)
-    folders = get_folders(data)
-    parents = check_parents(data, folders)
+    primary = get_primary(data)
+    folders = set_children(data, get_names(data), primary)
     with open(outputFile, 'w') as output:
         json.dump(folders,  output, sort_keys=True,
                   indent=4)
